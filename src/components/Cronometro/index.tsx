8318239864b0df225431b9741f632ed9eea83f05
Marcos Laurindo Ferreira <marcos_laurindo1997@hotmail.com>
2023-01-21 22:25:52 -0300

    update

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,31 @@
+import Botao from "../Botao";
+import Relogio from "./Relogio";
+import style from "./Cronometro.module.scss";
+import { tempoParaSegundos } from "../../common/utils/time";
+import { ITarefa } from "../../types/tarefa";
+import { useEffect, useState } from "react";
+
+interface Props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: Props) {
+  const [tempo, setTempo] = useState<number>();
+
+  useEffect(() => {
+    if(selecionado?.tempo){
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
+
+  return (
+    <div className={style.cronometro}>
+      <p className={style.titulo}>Escolha um card e inicie o cronômetro</p>
+      Tempo: {tempo}
+      <div className={style.relogioWrapper}>
+        <Relogio />
+      </div>
+      <Botao>Começar</Botao>
+    </div>
+  );
+}
---
+++ src/pages/App.tsx
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import Formulario from "../components/Formulario";
+import Lista from "../components/Lista";
+import Cronometro from "../components/Cronometro";
+import { ITarefa } from "../types/tarefa";
+import style from "./App.module.scss";
+
+function App() {
+  const [tarefas, setTarefas] = useState<ITarefa[]>([]);
+  const [selecionado, setSelecionado] = useState<ITarefa>();
+
+  function selecionaTarefa(tarefaSelecionada: ITarefa) {
+    setSelecionado(tarefaSelecionada);
+    setTarefas((tarefasAnteriores) =>
+      tarefasAnteriores.map((tarefa) => ({
+        ...tarefa,
+        selecionado: tarefa.id === tarefaSelecionada.id ? true : false,
+      }))
+    );
+  }
+
+  return (
+    <div className={style.AppStyle}>
+      <Formulario setTarefas={setTarefas} />
+      <Lista tarefas={tarefas} selecionaTarefa={selecionaTarefa} />
+      <Cronometro selecionado={selecionado} />
+    </div>
+  );
+}
+
+export default App;
